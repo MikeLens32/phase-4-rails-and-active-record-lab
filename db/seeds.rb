fcb7df3d4d64e5d55986d2d63104d61b8d986dab
Michael Dwyer <Mike.a.dwyer32@gmail.com>
2022-04-18 20:00:43 -0400

    commit tree 56dfae92fcf33e298c3445cf303d34fdc6955734

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating Example"
+
+Student.create([{first_name: "Dwayne", last_name: "Johnson", grade: 4}])
+
+puts "Example Complete"
